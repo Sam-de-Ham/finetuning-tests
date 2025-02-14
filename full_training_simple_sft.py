ca040ab77e627b90cf323d448db49d0258d64a92
Sam-de-Ham <weisz.sam321@gmail.com>
2025-02-14 10:08:09 +0000

    Cleanup

--- full_training_simple_sft.py
+++ full_training_simple_sft.py
@@ -7,18 +7,8 @@ from accelerate import Accelerator
 dataset = load_dataset("trl-lib/tldr", split="train")
 
 
-# Define the reward function, which rewards completions that are close to 20 characters
-def reward_len(completions, **kwargs):
-    return [-abs(20 - len(completion)) for completion in completions]
-
-
 training_args = SFTConfig(output_dir="Qwen-Distill-1.5B-GRPO")
-# trainer = GRPOTrainer(
-#     model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
-#     reward_funcs=reward_len,
-#     args=training_args,
-#     train_dataset=dataset,
-# )
+
 
 trainer = Accelerator().prepare(
     SFTTrainer(
